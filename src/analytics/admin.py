1eb69051f6c80077f6df7b12543645c4a888c6b5
Slavko <slavkopb@gmail.com>
2017-11-19 23:48:47 +0100

    Initial commit with working maps

--- src/analytics/admin.py
+++ src/analytics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Usersession
 
 # Register your models here.
+class IncidencesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+
+admin.site.register(Usersession, IncidencesAdmin)
--- src/analytics/migrations/0001_initial.py
+++ src/analytics/migrations/0001_initial.py
@@ -2,28 +2,21 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
+import django.contrib.gis.db.models.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='UserSession',
+            name='Usersession',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('session_key', models.CharField(max_length=60, null=True, blank=True)),
-                ('ip_address', models.GenericIPAddressField(null=True, blank=True)),
-                ('city_data', models.TextField(null=True, blank=True)),
-                ('city', models.CharField(max_length=120, null=True, blank=True)),
-                ('country', models.CharField(max_length=120, null=True, blank=True)),
-                ('active', models.BooleanField(default=True)),
-                ('timestamp', models.DateTimeField(auto_now_add=True)),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('name', models.CharField(max_length=20, null=True, blank=True)),
+                ('location', django.contrib.gis.db.models.fields.PointField(srid=4326)),
             ],
         ),
     ]
